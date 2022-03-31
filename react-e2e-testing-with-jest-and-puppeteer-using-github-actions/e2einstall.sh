78a1df3f1f02ec83622212b5ede34c3fed1a3655
Luciano <luciano.zapata@mentimeter.com>
2022-03-31 20:48:22 +0200

    feat

--- react-e2e-testing-with-jest-and-puppeteer-using-github-actions/e2einstall.sh
+++ react-e2e-testing-with-jest-and-puppeteer-using-github-actions/e2einstall.sh
@@ -1,1 +1,1 @@
-echo "sudo apt-get update" && echo "sudo apt-get install -y libgbm-dev" && echo "sudo apt-get install -y libxkbcommon-x11-0"
+sudo apt-get update && echo sudo apt-get install -y libgbm-dev && echo sudo apt-get install -y libxkbcommon-x11-0
