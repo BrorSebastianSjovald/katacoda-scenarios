b5ab86e390fa5066bc4a41c8dd01a51ab4646143
Luciano <luciano.zapata@mentimeter.com>
2022-03-31 20:50:05 +0200

    fix: fixed script

--- react-e2e-testing-with-jest-and-puppeteer-using-github-actions/e2einstall.sh
+++ react-e2e-testing-with-jest-and-puppeteer-using-github-actions/e2einstall.sh
@@ -1,1 +1,4 @@
-sudo apt-get update && echo sudo apt-get install -y libgbm-dev && echo sudo apt-get install -y libxkbcommon-x11-0
+#!/bin/bash
+sudo apt-get update  # To get the latest package lists
+sudo apt-get install -y libgbm-dev
+sudo apt-get install -y libxkbcommon-x11-0
